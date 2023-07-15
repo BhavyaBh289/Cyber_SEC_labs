724fcb9eced97becf71aad2ec52c02592c525f37
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-07-15 21:56:38 +0530

    all functions done but not working

--- Lab_2/S_des_encrypt.java
+++ Lab_2/S_des_encrypt.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 public class S_des_encrypt{
     static int[] p10 = new int[]{ 3,5,2,7,4,10,1,9,8,6 };
     static int[] p8 = new int[]{6,3,7,4,8,5,10,9}; 
+    static int[] ip = new int []{2,6,3,1,4,8,5,7};
+    static int[] ip1 = new int []{4,1,3,5,7,2,8,6};
+    static int[]  ep = new int []{4,1,2,3,2,3,4,1};
+    static int[] p4 = new int []{2,4,3,1};
+    static int[] []s0 = new int [][]{{1,0,3,2},{3,2,1,0},{0,2,1,3},{3,1,3,0}};
+    static int[] []s1 = new int [][]{{0,1,2,3},{2,0,1,3},{3,0,1,0},{2,1,0,3}};
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] splitArray = sc.nextLine().split(" ");
@@ -11,31 +18,44 @@ public class S_des_encrypt{
         for (int i = 0; i < splitArray.length; i++) {
             key[i] = Integer.parseInt(splitArray[i]);
         }
-        int[][] keys = keygen(key);
-        System.out.print(Arrays.deepToString(keys));     
+        // System.out.print(Arrays.deepToString(keys));     
         String[] splitArrayy = sc.nextLine().split(" ");
         int[] plaintext = new int[splitArrayy.length];
         for (int i = 0; i < splitArrayy.length; i++) {
             plaintext[i] = Integer.parseInt(splitArrayy[i]);
         }
-        // for (int i=0;i<array.length;i++){
-        //     System.out.print(array[i]);
-        // }
-        // int [] data = permutate(array, arrayy);
-        // for (int i=0;i<data.length;i++){
-        //     System.out.print(data[i]);
-        // }
-        // ls(1,input);
-        // for (int i=0;i<len;i++){
-        //     System.out.print(input[i]);
-        // }
+        int [] cyphertext = kx(key, plaintext);
+        for (int i :cyphertext){
+            System.out.print(i);
+        }
         sc.close();
     }
     public static int[] kx (int[]key,int[]data){
-        int [] ne = new int [5];
-        return ne;
+        int[][] keys = keygen(key);
+        int [] data1 = permutate(ip, data);
+        data1 = rx(keys[0],data1);
+        ls(4,data1);
+        data1 = rx(keys[1],data1);
+        data1 = permutate(ip, data);
+        return data1;
     }
-
+    public static int[] rx (int[]key,int[]data){
+        int [] n1 =Arrays.copyOfRange(data ,0,4);
+        int [] n2 =Arrays.copyOfRange(data ,4,data.length);
+        int [] sol = new int [8];
+        System.arraycopy(n2, 0,sol,4 ,4);
+        int[] w1 = permutate(ep, n2);
+        w1 = xor(w1,key);
+        int [] n3 =Arrays.copyOfRange(w1,0,4);
+        int [] n4 =Arrays.copyOfRange(w1 ,4,data.length);
+        System.arraycopy(substitute(s0, n3), 0,n2,0 ,2);
+        System.arraycopy(substitute(s1, n4), 0,n2,2 ,2);
+        n2 = permutate(p4,n2);
+        n2 = xor(n1, n2);
+        System.arraycopy(n2, 0,sol,0 ,4);
+        return sol;
+    }
+    
     public static int[][] keygen (int[] key){
         int [] data = permutate(p10,key );
         int [][] finalkeys = new int[2][];
@@ -71,7 +91,6 @@ public class S_des_encrypt{
             inp[i]= ne[i];
         }
     }
-
     public static int[] xor (int []arr1 , int[] arr2){
         int [] result = new int[arr1.length];
         for (int i = 0; i < arr1.length; i++) {
@@ -79,8 +98,18 @@ public class S_des_encrypt{
         }
         return result;
     }
-
-    public static int[] substitute(int[][]matrix,int[]value){
-        
+    public static int toint(int t, int q){
+        int n = t;
+        return n+=q*2;
     }
+    public static int []toarr(int t){
+        int[] n = new int []{ t/2,t%2};
+        return n;
+    }
+    public static int[] substitute(int[][]matrix,int[]value){
+        int [] arr = toarr(matrix[toint(value[0], value[3])][toint(value[1], value[2])]) ;
+        return arr;
+    }
+
+
 }
